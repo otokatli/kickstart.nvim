2096c230928f4314f79557c1dcd81aaba1fe480d
Ozan Tokatli <ozan.tokatli@gmail.com>
2026-03-26 16:21:05 +0000

    added tex specific config

---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,12 @@
+-- Convert tabs to 2 spaces
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+
+-- Enable auto-indentation
+vim.opt_local.autoindent = true
+vim.opt_local.smartindent = true
+
+-- Optional: Enable spell checking for LaTeX files, which is usually super helpful
+vim.opt_local.spell = true
+vim.opt_local.spelllang = 'en_gb'
